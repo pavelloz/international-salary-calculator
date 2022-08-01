3579832ebed0e221b9ff2066d183648b0f6dd32d
Pawel Kowalski <pavelloz@gmail.com>
2022-08-01 02:26:13 +0200

    Add edge function to get rates

---
+++ pages/api/getRates.js
@@ -0,0 +1,32 @@
+export const config = {
+  runtime: "experimental-edge",
+};
+
+const CURRENCIES = ["usd", "gbp", "eur", "chf"];
+
+export default async function handler(_req) {
+  const url =
+    "https://api.nbp.pl/api/exchangerates/tables/a/last/1/?format=json";
+
+  const data = await fetch(url)
+    .then((data) => data.json())
+    .then((data) => data[0]?.rates)
+    .then((rates) => {
+      const output = rates.reduce((prev, cur) => {
+        if (!CURRENCIES.includes(cur.code.toLowerCase())) return { ...prev };
+        let { code, mid } = cur;
+        return { ...prev, [code.toLowerCase()]: parseFloat(mid.toFixed(3)) };
+      }, {});
+      return output;
+    });
+
+  return new Response(
+    JSON.stringify(data, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "public, s-maxage=1200, stale-while-revalidate=3600",
+      },
+    })
+  );
+}
