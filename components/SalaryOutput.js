946980d5f49d92f9e10c0f3790e9253157b54d3a
Pawel Kowalski <pavelloz@gmail.com>
2026-02-09 00:19:11 +0100

    Visualize as table

--- components/SalaryOutput.js
+++ components/SalaryOutput.js
@@ -25,29 +25,40 @@ export default () => {
     <>
       <h3 className="w-full">Salary in PLN</h3>
 
-      <h4>Gross</h4>
-      <ul className="md:flex justify-between flex-wrap list-none pl-0!">
-        <li className="pl-0">{formatSalary(salaries.hourly)} / hour</li>
-        <li className="pl-0">{formatSalary(salaries.daily)} / day</li>
-        <li className="pl-0">{formatSalary(salaries.monthly)} / month</li>
-        <li className="pl-0">{formatSalary(salaries.yearly)} / year</li>
-      </ul>
-
-      <h4>B2B LumpSum tax (12% PIT, big ZUS)</h4>
-      <ul className="md:flex justify-between flex-wrap list-none pl-0!">
-        <li className="pl-0">{formatSalary(flatTax12.hourly)} / hour</li>
-        <li className="pl-0">{formatSalary(flatTax12.daily)} / day</li>
-        <li className="pl-0">{formatSalary(flatTax12.monthly)} / month</li>
-        <li className="pl-0">{formatSalary(flatTax12.yearly)} / year</li>
-      </ul>
-
-      <h4>B2B Linear tax (19% PIT, big ZUS)</h4>
-      <ul className="md:flex justify-between flex-wrap list-none pl-0!">
-        <li className="pl-0">{formatSalary(linearTax19.hourly)} / hour</li>
-        <li className="pl-0">{formatSalary(linearTax19.daily)} / day</li>
-        <li className="pl-0">{formatSalary(linearTax19.monthly)} / month</li>
-        <li className="pl-0">{formatSalary(linearTax19.yearly)} / year</li>
-      </ul>
+      <table class="w-full table-auto">
+        <thead>
+          <tr className="text-left">
+            <th></th>
+            <th>Hourly</th>
+            <th>Daily</th>
+            <th>Monthly</th>
+            <th>Yearly</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-gray-600">
+            <td>Gross</td>
+            <td>{formatSalary(salaries.hourly)}</td>
+            <td>{formatSalary(salaries.daily)}</td>
+            <td>{formatSalary(salaries.monthly)}</td>
+            <td>{formatSalary(salaries.yearly)}</td>
+          </tr>
+          <tr className="border-t border-gray-400">
+            <td>19% Linear tax (big ZUS)</td>
+            <td>{formatSalary(linearTax19.hourly)}</td>
+            <td>{formatSalary(linearTax19.daily)}</td>
+            <td>{formatSalary(linearTax19.monthly)}</td>
+            <td>{formatSalary(linearTax19.yearly)}</td>
+          </tr>
+          <tr className="">
+            <td>12% Flat rate tax (big ZUS)</td>
+            <td>{formatSalary(flatTax12.hourly)}</td>
+            <td>{formatSalary(flatTax12.daily)}</td>
+            <td>{formatSalary(flatTax12.monthly)}</td>
+            <td>{formatSalary(flatTax12.yearly)}</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
