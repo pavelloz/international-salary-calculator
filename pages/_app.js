83f31a1d07c7451885c50b310cfee4655259f483
Pawel Kowalski <pavelloz@gmail.com>
2026-02-09 10:30:14 +0100

    Revert "Fix warning"
    This reverts commit d4db2df92b4438326ef388a06473aa9d1501facd.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import "../styles/globals.css";
-import { Head } from "next/document";
 
 export default ({ Component, pageProps }) => {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(
-    <>
-      <Head>
-        <title>Salary Calculator for 🇵🇱 folks</title>
-      </Head>
-
-      <Component {...pageProps} />
-    </>,
-  );
+  return getLayout(<Component {...pageProps} />);
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,11 @@
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
+      <Head>
+        <title>Salary Calculator for 🇵🇱 folks</title>
+      </Head>
       <body className="bg-gray-100 pt-8">
         <Main />
         <NextScript />
