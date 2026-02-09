7ac9b7b9e34146767262fb9be8bae0aa9408b39a
Pawel Kowalski <pavelloz@gmail.com>
2026-02-09 10:37:41 +0100

    Add persistent zustand persistent storage

--- stores/useRatesStore.js
+++ stores/useRatesStore.js
@@ -1,16 +1,25 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const useRatesStore = create()((set) => ({
-  rates: {},
-  fetchedAt: +new Date(),
-  salary: 10000,
-  currency: "usd",
-  period: "monthly",
-  setRates: (rates) => set({ rates }),
-  setFetchedAt: (fetchedAt) => set({ fetchedAt }),
-  setSalary: (salary) => set({ salary }),
-  setCurrency: (currency) => set({ currency }),
-  setPeriod: (period) => set({ period }),
-}));
+const useRatesStore = create()(
+  persist(
+    (set) => ({
+      rates: {},
+      fetchedAt: +new Date(),
+      salary: 10000,
+      currency: "usd",
+      period: "monthly",
+      setRates: (rates) => set({ rates }),
+      setFetchedAt: (fetchedAt) => set({ fetchedAt }),
+      setSalary: (salary) => set({ salary }),
+      setCurrency: (currency) => set({ currency }),
+      setPeriod: (period) => set({ period }),
+    }),
+    {
+      name: "user-input",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
 
 export default useRatesStore;
