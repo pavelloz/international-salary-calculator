4934f1569afd74c55933777821d36d1b341d844f
Pawel Kowalski <pavelloz@gmail.com>
2026-02-15 21:20:36 +0100

    Experiment with goldprice

--- app/home-client.js
+++ app/home-client.js
@@ -19,6 +19,8 @@ export default function HomePage({ rates }) {
 
   return (
     <>
+      {/* <h4>Gold price: {goldPrice}</h4> */}
+
       <SalaryInput />
       <SalaryOutput />
       <ExchangeRatesList />
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import { fetchRates } from "@/lib/fx-rates";
+import { fetchGoldPrice } from "@/lib/gold-price";
 import HomeClient from "./home-client";
 
 export default async function HomePage() {
   const rates = await fetchRates();
+  const goldPrice = await fetchGoldPrice();
 
-  return <HomeClient rates={rates} />;
+  return <HomeClient rates={rates} goldPrice={goldPrice} />;
 }
