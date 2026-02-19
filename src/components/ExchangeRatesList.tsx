b7db9e98f80ba6b2547624e4e45394922a1cbc9b
Pawel Kowalski <pavelloz@gmail.com>
2026-02-19 20:17:58 +0100

    Fix race condition

--- src/components/ExchangeRatesList.tsx
+++ src/components/ExchangeRatesList.tsx
@@ -7,13 +7,17 @@ export default function ExchangeRatesList() {
   const { currency } = useStore($userInputStore);
   const { rates } = useStore($ratesStore);
 
-  const selectedRate = rates[currency];
+  const selectedRate = rates && currency ? rates[currency] : null;
+
+  if (!selectedRate) return null;
 
   return (
     <div className="border-t border-gray-500 mt-4 pt-4">
-      <p className="pt-0 italic text-sm">
-        1 PLN = {selectedRate} {currency.toUpperCase()}
-      </p>
+      {currency !== "pln" && (
+        <p className="pt-0 italic text-sm">
+          1 PLN = {selectedRate} {currency.toUpperCase()}
+        </p>
+      )}
     </div>
   );
 }
