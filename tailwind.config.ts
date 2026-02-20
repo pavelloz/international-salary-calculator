812f870bce0d212ca02324af7d9d79b4f0820145
Pawel Kowalski <pavelloz@gmail.com>
2026-02-20 10:17:13 +0100

    Fix vite inconsistent versions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+import forms from "@tailwindcss/forms";
+export default {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [forms],
 };
