4679fa5e59911ae1168828d0302c34515495d3f2
Pawel Kowalski <pavelloz@gmail.com>
2026-02-25 16:56:08 +0100

    Fix TS

--- src/components/SalaryInput.tsx
+++ src/components/SalaryInput.tsx
@@ -13,28 +13,22 @@ export default function SalaryInput() {
     <div class="flex justify-between flex-wrap pb-8 gap-x-6">
       <h3 class="w-full text-gray-700 mt-0">Salary in foreign currency</h3>
 
-      <select class="flex-1" id="period" onChange={(e) => setPeriod(e.currentTarget.value)} value={store().period}>
-        <For each={PERIODS}>
-          {(p: string) => (
-            <option value={p}>
-              {p.charAt(0).toUpperCase() + p.slice(1)}
-            </option>
-          )}
-        </For>
+      <select class="flex-1" id="period" onChange={e => setPeriod(e.currentTarget.value)} value={store().period}>
+        <For each={PERIODS}>{(p: string) => <option value={p}>{p.charAt(0).toUpperCase() + p.slice(1)}</option>}</For>
       </select>
       <input
         class="flex-1"
         id="salary"
         name="salary"
         value={store().salary}
-        onInput={(e) => {
+        onInput={e => {
           const cleanValue = cleanNumericInput(e.currentTarget.value);
           e.currentTarget.value = cleanValue;
           setSalary(cleanValue);
         }}
         pattern="\d*"
       />
-      <select class="flex-0" id="currency" value={store().currency} onChange={(e) => setCurrency(e.currentTarget.value)}>
+      <select class="flex-0" id="currency" value={store().currency} onChange={e => setCurrency(e.currentTarget.value)}>
         <For each={CURRENCIES}>
           {(c: string) => (
             <option value={c}>
@@ -50,7 +44,7 @@ export default function SalaryInput() {
         id="daysOff"
         name="daysOff"
         value={store().daysOff}
-        onInput={(e) => {
+        onInput={e => {
           const cleanValue = cleanNumericInput(e.currentTarget.value);
           e.currentTarget.value = cleanValue;
           setDaysOff(cleanValue);
