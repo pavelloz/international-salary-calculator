2072187b3f0505611fefe6b9d2a7aa260fddefee
Pawel Kowalski <pavelloz@gmail.com>
2026-02-25 20:21:17 +0100

    Chunking experiments

--- astro.config.ts
+++ astro.config.ts
@@ -8,5 +8,27 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'js/[name].[hash].js',
+          chunkFileNames: 'js/[name].[hash].js',
+          assetFileNames: '[ext]/[name].[hash].[ext]',
+          experimentalMinChunkSize: 10000, // Merges chunks under 10kB
+          manualChunks(id) {
+            // Group solid-js, nanostores, and valibot together
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+
+            if (id.includes('/src/')) {
+              return 'app';
+            }
+          }
+        }
+      }
+    }
+  }
 });
