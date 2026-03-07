1a230754cb142bb5cacd13db277f6de362257751
Pawel Kowalski <pavelloz@gmail.com>
2026-03-07 14:17:48 +0100

    Cleanup tailwind config and TS e

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import forms from "@tailwindcss/forms";
-export default {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [forms],
-};
